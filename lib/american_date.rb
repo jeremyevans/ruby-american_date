570e026081fd9da960049861b307b3cc503f5f7c
Sebastian Delmont <sd@notso.net>
2013-03-22 15:16:24 -0400

    use negative lookahead/behind in the regexp to prevent matching non-dates

--- lib/american_date.rb
+++ lib/american_date.rb
@@ -4,7 +4,10 @@ if RUBY_VERSION >= '1.9'
   # Modify parsing methods to handle american date format correctly.
   class << Date
     # American date format detected by the library.
-    AMERICAN_DATE_RE = %r_\D(\d{1,2})/(\d{1,2})/(\d{4}|\d{2})_.freeze
+    AMERICAN_DATE_RE = eval('%r_(?<!\d)(\d{1,2})/(\d{1,2})/(\d{4}|\d{2})(?!\d)_').freeze
+    # Negative lookbehinds, which are not supported in Ruby 1.8
+    # so by using eval, we prevent an error when this file is first parsed
+    # since the regexp itself will only be parsed at runtime if the RUBY_VERSION condition is met.
 
     # Alias for stdlib Date._parse
     alias _parse_without_american_date _parse
