2168ce5f1c1969edacc047eeb5a221cf07ebfba5
Jeremy Evans <code@jeremyevans.net>
2023-07-07 08:16:46 -0700

    Make sure AMERICAN_DATE_RE constant is defined on Date and not on the top level

--- lib/american_date.rb
+++ lib/american_date.rb
@@ -14,13 +14,14 @@ if RUBY_VERSION >= '1.9'
   # :nocov:
   end
 
+  # American date format detected by the library.
+  Date::AMERICAN_DATE_RE = eval('%r_(?<!\d)(\d{1,2})/(\d{1,2})/(\d{4}|\d{2})(?!\d)_').freeze
+  # Negative lookbehinds, which are not supported in Ruby 1.8
+  # so by using eval, we prevent an error when this file is first parsed
+  # since the regexp itself will only be parsed at runtime if the RUBY_VERSION condition is met.
+
   # Modify parsing methods to handle american date format correctly.
   Date.instance_eval do
-    # American date format detected by the library.
-    AMERICAN_DATE_RE = eval('%r_(?<!\d)(\d{1,2})/(\d{1,2})/(\d{4}|\d{2})(?!\d)_').freeze
-    # Negative lookbehinds, which are not supported in Ruby 1.8
-    # so by using eval, we prevent an error when this file is first parsed
-    # since the regexp itself will only be parsed at runtime if the RUBY_VERSION condition is met.
 
     # Alias for stdlib Date._parse
     alias _parse_without_american_date _parse
@@ -77,7 +78,7 @@ if RUBY_VERSION >= '1.9'
           raise TypeError, "no implicit conversion of #{string.inspect} into String"
         end
       end
-      string.sub(AMERICAN_DATE_RE){|m| "#$3-#$1-#$2"}
+      string.sub(Date::AMERICAN_DATE_RE){|m| "#$3-#$1-#$2"}
     end
   end
 
